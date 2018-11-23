b81490bdb097e591722496d4bc535ee6419b9eba
mp-17 <twisted@live.it>
2018-11-23 17:40:43 +0100

    added timings

--- vhdl_material/full_project/vhdl/src/addSub.vhd
+++ vhdl_material/full_project/vhdl/src/addSub.vhd
@@ -14,16 +14,20 @@ end entity;
 
 architecture beh of addSub is
 
-	signal 
+	signal add0Ext, add1Ext: std_logic_vector(nBit downto 0);
 
 begin
+
+add0Ext <= add0(nBit-1) & add0;
+
+add1Ext <= add1(nBit-1) & add1;
 
 process(sub, add0, add1)
 begin
 	if (sub = '0') then
-		sum <= std_logic_vector( signed(add0) + signed(add1) );
+		sum <= std_logic_vector( signed(add0Ext) + signed(add1Ext) );
 	else
-		sum <= std_logic_vector( signed(add0) - signed(add1) );
+		sum <= std_logic_vector( signed(add0Ext) - signed(add1Ext) );
 	end if;
 end process;
 
